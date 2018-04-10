293b17eb27158fdbfa723ecd1c65ff25f504e2d2
funyug <shivamchawla243@gmail.com>
2018-04-10 17:49:03 +0530

    Tutorial 2

---
+++ tutorial2/consignment-service/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	pb "go-microservices-tutorials/tutorial2/consignment-service/proto/consignment"
+	vesselProto "github.com/EwanValentine/shippy/vessel-service/proto/vessel"
+	"github.com/micro/go-micro"
+	"context"
+	"fmt"
+	"log"
+)
+
+type Repository interface {
+	Create(*pb.Consignment) (*pb.Consignment,error)
+	GetAll() []*pb.Consignment
+}
+
+// Repository - Dummy repository, this simulates the use of a datastore
+// of some kind. We'll replace this with a real implementation later on.
+type ConsignmentRepository struct {
+	consignments []*pb.Consignment
+}
+
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment,error) {
+	updated := append(repo.consignments,consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo Repository
+	vesselClient vesselProto.VesselServiceClient
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	vesselResponse,err := s.vesselClient.FindAvailable(context.Background(),&vesselProto.Specification{
+		MaxWeight:req.Weight,
+		Capacity:int32(len(req.Containers)),
+	})
+	if err != nil {
+		return err
+	}
+	log.Printf("Found vessel: %s \n",vesselResponse.Vessel.Name)
+
+	req.VesselId = vesselResponse.Vessel.Id
+
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+
+	res.Created = true
+	res.Consignment = consignment
+	return nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
+}
+
+func main() {
+	repo := &ConsignmentRepository{}
+
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.consignment"),
+		micro.Version("latest"),
+	)
+
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel",srv.Client())
+
+	srv.Init()
+
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo,vesselClient})
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
---
+++ tutorial2/vessel-service/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	pb "go-microservices-tutorials/tutorial2/vessel-service/proto/vessel"
+	"errors"
+	"context"
+	"github.com/micro/go-micro"
+	"fmt"
+)
+
+type Repository interface {
+	FindAvailable(*pb.Specification) (*pb.Vessel,error)
+}
+
+type VesselRepository struct {
+	vessels []*pb.Vessel
+}
+
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel,error){
+	for _,vessel := range repo.vessels {
+		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
+			return vessel,nil
+		}
+	}
+	return nil, errors.New("No vessel found by spec")
+}
+
+type service struct {
+	repo Repository
+}
+
+func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
+	vessel,err:=s.repo.FindAvailable(req)
+	if err != nil {
+		return err
+	}
+	res.Vessel = vessel
+	return nil
+}
+
+func main() {
+	vessels := []*pb.Vessel{
+		&pb.Vessel{Id:"vessel01",Name:"Boaty McBoatface",MaxWeight:2000,Capacity:500},
+	}
+	repo:=&VesselRepository{vessels}
+
+	srv:=micro.NewService(
+		micro.Name("go.micro.srv.vessel"),
+		micro.Version("latest"),
+	)
+
+	srv.Init()
+
+	pb.RegisterVesselServiceHandler(srv.Server(),&service{repo})
+
+	if err:=srv.Run();err != nil {
+		fmt.Println(err)
+	}
+}
